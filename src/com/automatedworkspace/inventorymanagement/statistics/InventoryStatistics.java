ba5bd272e9c494bb28aa8800495188ef02b63079
Invop <shandrykjj512@gmail.com>
2023-03-16 19:13:33 +0200

    test

--- src/com/automatedworkspace/inventorymanagement/statistics/InventoryStatistics.java
+++ src/com/automatedworkspace/inventorymanagement/statistics/InventoryStatistics.java
@@ -1,4 +1,41 @@
 package com.automatedworkspace.inventorymanagement.statistics;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+
+import java.io.*;
+
+
 public class InventoryStatistics {
+
+
+    public static void main(String[] args) throws Exception {
+
+        String filePath = "src/com/automatedworkspace/files/Inventory.xlsx";
+
+        Workbook workbook = WorkbookFactory.create(new File(filePath));
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 2; col <= 4; col++) {
+                Cell cell = sheet.getRow(row).getCell(col);
+                if (cell != null) {
+                    System.out.print(cell.getStringCellValue() + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+        workbook.close();
+    }
+
 }
