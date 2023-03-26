6812c2a86c767908bbefa2a03effd35ada48b909
Anna <tronana72@gmail.com>
2023-03-27 02:07:01 +0300

    Add panel for AddToExistingForm

--- src/com/automatedworkspace/inventorymanagement/ui/AddItem/AddToExistingForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/AddItem/AddToExistingForm.java
@@ -1,4 +1,16 @@
 package com.automatedworkspace.inventorymanagement.ui.AddItem;
 
+import javax.swing.*;
+
 public class AddToExistingForm {
+    private JPanel PanelAddForm;
+    private JLabel LabelAddForm;
+    private JLabel LabelChooseForm;
+    private JComboBox ChooseComboBox;
+    private JLabel LabelNumber;
+    private JTextField NumberField;
+    private JLabel IntervalLabel;
+    private JTextField IntervalField;
+    private JButton OKButton;
+    private JButton buttonCancel;
 }
