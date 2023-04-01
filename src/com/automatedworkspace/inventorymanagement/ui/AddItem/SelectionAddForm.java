90a0d1bc7f83005fa3db3ac82917197c333f4e55
Invop <shandrykjj512@gmail.com>
2023-04-01 23:17:26 +0300

    SelectionAddForm.java fix

--- src/com/automatedworkspace/inventorymanagement/ui/AddItem/SelectionAddForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/AddItem/SelectionAddForm.java
@@ -1,4 +1,11 @@
 package com.automatedworkspace.inventorymanagement.ui.AddItem;
 
+import javax.swing.*;
+
 public class SelectionAddForm {
+	private JButton homeBtn;
+	private JPanel SelectionAddPanel;
+	private JButton addGroupBtn;
+	private JButton addSupplierBtn;
+	private JButton addItemBtn;
 }
