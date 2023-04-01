cdde1bf991464569f2754480597b8a0b3389cef9
Anna <tronana72@gmail.com>
2023-04-01 10:19:21 +0300

    Add panel for AddSupplierForm and AddGroupForm

--- src/com/automatedworkspace/inventorymanagement/ui/AddItem/AddSupplierForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/AddItem/AddSupplierForm.java
@@ -1,4 +1,14 @@
 package com.automatedworkspace.inventorymanagement.ui.AddItem;
 
+import javax.swing.*;
+
 public class AddSupplierForm {
+    private JPanel AddSupplierPanel;
+    private JLabel AddSupplierLabel;
+    private JLabel NameSupplierLabel;
+    private JTextField NameSupplierField;
+    private JButton CancelAddSupplierButton;
+    private JButton OKAddSupplierButton;
+
+
 }
