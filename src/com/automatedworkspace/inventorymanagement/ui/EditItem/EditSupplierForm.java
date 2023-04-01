0f0d58c6931b306c2ba3f293ebaa7d514a904898
Invop <shandrykjj512@gmail.com>
2023-04-02 00:27:53 +0300

    ads

--- src/com/automatedworkspace/inventorymanagement/ui/EditItem/EditSupplierForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/EditItem/EditSupplierForm.java
@@ -1,4 +1,13 @@
 package com.automatedworkspace.inventorymanagement.ui.EditItem;
 
+import javax.swing.*;
+
 public class EditSupplierForm {
+	private JComboBox comboBox1;
+	private JButton supplierOkButton;
+	private JTextField supplierNameField;
+	private JLabel supplierNameLabel;
+	private JComboBox supplierComboBox;
+	private JPanel editSupplierPanel;
+	private JButton supplierCancelButton;
 }
