964a3d5f5423a1d5d7f13862ac6103cf120173bc
Invop <shandrykjj512@gmail.com>
2023-04-01 23:23:51 +0300

    SelectionDeleteForm.java add fields

--- src/com/automatedworkspace/inventorymanagement/ui/DeleteItem/SelectionDeleteForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/DeleteItem/SelectionDeleteForm.java
@@ -1,4 +1,11 @@
 package com.automatedworkspace.inventorymanagement.ui.DeleteItem;
 
+import javax.swing.*;
+
 public class SelectionDeleteForm {
+	private JPanel SelectionDeletePanel;
+	private JButton DeleteItemButton;
+	private JButton DeleteGroupButton;
+	private JButton DeleteSupplierButton;
+	private JButton HomeButton;
 }
