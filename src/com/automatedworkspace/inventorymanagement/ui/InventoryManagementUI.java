b00efd71eac3ef416102496e4bda7b800d4bdb10
Invop <shandrykjj512@gmail.com>
2023-04-07 21:13:28 +0300

    Main ui form

--- src/com/automatedworkspace/inventorymanagement/ui/InventoryManagementUI.java
+++ src/com/automatedworkspace/inventorymanagement/ui/InventoryManagementUI.java
@@ -2,14 +2,25 @@ package com.automatedworkspace.inventorymanagement.ui;
 
 import javax.swing.*;
 
-public class InventoryManagementUI extends JDialog{
-    private JPanel InventoryManagementPanel;
-
-	public InventoryManagementUI(JFrame parent){
-        super(parent);
-	    setVisible(true);
-	    setSize(400, 300);
-	    setContentPane(InventoryManagementPanel);
-    }
-
+public class InventoryManagementUI {
+	private JPanel MainPanel;
+	private JTable table;
+	private JButton addButton;
+	private JButton editButton;
+	private JButton deleteButton;
+	private JButton importButton;
+	private JButton exportButton;
+	private JLabel searchLabel;
+	private JButton findButton;
+	private JRadioButton byGroupRadioButton;
+	private JRadioButton byNameRadioButton;
+	private JRadioButton byIdRadioButton;
+	private JRadioButton bySupplierRadioButton;
+	private JTextField textFieldNameId;
+	private JComboBox comboBoxGroupSupplier;
+	private JRadioButton byPriceFromToRadioButton;
+	private JTextField fromField;
+	private JTextField toField3;
+	private JPanel ProductSearchPanel;
+	private JPanel ButtonsPanel;
 }
