db9034ce1a2d95cb569f99a3a67c9188d04fb328
Invop <shandrykjj512@gmail.com>
2023-04-11 13:19:26 +0300

    Delete Supplier fix

--- src/com/automatedworkspace/inventorymanagement/ui/DeleteItem/DeleteSupplierForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/DeleteItem/DeleteSupplierForm.java
@@ -2,12 +2,17 @@ package com.automatedworkspace.inventorymanagement.ui.DeleteItem;
 
 import com.automatedworkspace.inventorymanagement.statistics.Config;
 import com.automatedworkspace.inventorymanagement.statistics.ConfigManager;
+import org.apache.poi.ss.usermodel.*;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+
+import static com.automatedworkspace.inventorymanagement.ui.AddItem.AddItemForm.EXEL_FILE_PATH;
 
 public class DeleteSupplierForm extends JDialog{
     private JPanel DeleteSuplplierPanel;
@@ -69,22 +74,40 @@ public class DeleteSupplierForm extends JDialog{
 
     }
     private void deleteSupplier() throws IOException{
+        FileInputStream filePath = new FileInputStream(EXEL_FILE_PATH);
+        Workbook workbook = WorkbookFactory.create(filePath);
+        Sheet sheet = workbook.getSheetAt(0);
+
         Config config = ConfigManager.readConfig();
         List<String> suppList= config.getSupplierList();
         List<Integer> itemSuppList = config.getItemSupplierList();
         int selectedIndx = DeleteSupplierComboBox.getSelectedIndex();
         for (int i = itemSuppList.size() - 1; i >= 0; i--){
-            if (itemSuppList.get(i)!=-1 && itemSuppList.get(i)!=selectedIndx){
+            if (itemSuppList.get(i)!=-1 && itemSuppList.get(i)>selectedIndx){
                 if(itemSuppList.get(i)!=0) {
                     itemSuppList.set(i, itemSuppList.get(i) - 1);
                 }
             }
             else if(itemSuppList.get(i)!=-1 && itemSuppList.get(i)==selectedIndx){
                 itemSuppList.set(i,-1);
+                for (int j = 3; j <= config.getNotNullRows(); j++) {
+                    Row row = sheet.getRow(j);
+                    Cell cell = row.getCell(4);
+                    if(cell.getStringCellValue().equals(suppList.get(selectedIndx))){
+                        cell.setCellValue("null");
+                    }
+                }
+
             }
+
         }
+        FileOutputStream out = new FileOutputStream(EXEL_FILE_PATH);
+        workbook.write(out);
+        out.close();
+        workbook.close();
         suppList.remove(selectedIndx);
         ConfigManager.writeConfig(config);
+
     }
 
 
