c2bc002361e6c56260e0c8132019bd760b2f9efd
Anna <tronana72@gmail.com>
2023-04-15 11:24:34 +0300

    1

--- src/com/automatedworkspace/inventorymanagement/ui/EditItem/EditSupplierForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/EditItem/EditSupplierForm.java
@@ -10,4 +10,5 @@ public class EditSupplierForm {
 	private JComboBox supplierComboBox;
 	private JPanel editSupplierPanel;
 	private JButton supplierCancelButton;
+
 }
--- src/com/automatedworkspace/inventorymanagement/ui/EditItem/SelectionEditForm.java
+++ src/com/automatedworkspace/inventorymanagement/ui/EditItem/SelectionEditForm.java
@@ -1,11 +1,61 @@
 package com.automatedworkspace.inventorymanagement.ui.EditItem;
 
-import javax.swing.*;
+import com.automatedworkspace.inventorymanagement.ui.DeleteItem.DeleteGroupForm;
+import com.automatedworkspace.inventorymanagement.ui.DeleteItem.DeleteItemForm;
+import com.automatedworkspace.inventorymanagement.ui.DeleteItem.DeleteSupplierForm;
+import com.automatedworkspace.inventorymanagement.ui.InventoryManagementUI;
 
-public class SelectionEditForm {
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class SelectionEditForm extends JDialog {
 	private JPanel EditSelectionPanel;
 	private JButton EditItemButton;
 	private JButton EditGroupButton;
 	private JButton EditSupplierButton;
 	private JButton HomeButton;
+	public SelectionEditForm(JFrame parent) {
+		super(parent);
+		setVisible(true);
+		setSize(400, 300);
+		setContentPane(EditSelectionPanel);
+		setLocationRelativeTo(parent);
+		IfEditItemButton();
+		IfEditGroupButton();
+		IfDeleteSupplierButton();
+		IfhomeBtnPressed();
+		CloseApp();
+	}
+	private void IfEditItemButton(){
+		EditItemButton.addActionListener(e -> {
+			dispose();
+		});
+	}
+	private void IfEditGroupButton() {
+		EditGroupButton.addActionListener(e -> {
+			dispose();
+			new EditGroupForm(null);
+		});
+	}
+	private void IfDeleteSupplierButton() {
+		EditSupplierButton.addActionListener(e -> {
+			dispose();
+		});
+	}
+	private void CloseApp() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+			}
+		});
+	}
+	private void IfhomeBtnPressed() {
+		HomeButton.addActionListener(e -> {
+			dispose();
+			new InventoryManagementUI(null);
+		});
+	}
 }
